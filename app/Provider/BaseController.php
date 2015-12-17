dcfcf53f51cf7adf24595b1d29494f13bde15a24
Franck Dakia <dakiafranck@gmail.com>
2015-12-17 23:14:29 +0000

    Ajout de la fonction middleware en BaseController

--- app/Provider/BaseController.php
+++ app/Provider/BaseController.php
@@ -4,5 +4,26 @@ namespace App\Provider;
 
 class BaseController
 {
+	/**
+	 * 
+	 * @var string
+	 */
+	protected $middleware_namespace = "App\Http\Middleware\\"; 
+
+	/**
+	 * Lanceur de middleware
+	 * 
+	 * @param string $name
+	 * @param mixed
+	 * @return mixed
+	 */
+	public function middleware($name, $data)
+	{
+		$middleware = $this->middleware_namespace . $name;
+		if (class_exists($middleware)) {
+			$class = new $middleware();
+			return call_user_func_array([$class, "hanlder", $data);
+		}
+	}
 
 }
