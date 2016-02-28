2c7db7b7e02120a1ee8859f953c25aced60fc30d
Franck Dakia <dakiafranck@gmail.com>
2016-02-28 21:37:37 +0000

    config:changement des noms de clé

--- config/application.php
+++ config/application.php
@@ -11,17 +11,17 @@
 
 return (object) [
     // Nom de l'Application
-	"appname" => "The Bow Framework",
+	"app_name" => "The Bow Framework",
     // La local
 	"timezone" => "Africa/Abidjan",
     // Niveau de log
-	"loglevel" => "dev",
+	"log_level" => "dev",
     // Repertoire de log
-    "logDirecotoryName" => dirname(__DIR__) . "/storage/logs",
+    "log_direcotory_name" => dirname(__DIR__) . "/storage/logs",
     // En cas d'utilisation de token. Ceci est le temps de vie d'un token
 	"tokenExpirateTime" => 50000,
     // chemin du fichier 404
-	"notFoundFileName" => dirname(__DIR__) . "/app/views/404.php",
+	"not_found_file_name" => dirname(__DIR__) . "/app/views/404.php",
     // Template par defaut utiliser par Snoop. Le systeme implement 3 moteur de template
     // Valeur possible: twig, mustache, jade
   	"template" => "twig",
--- config/resource.php
+++ config/resource.php
@@ -1,27 +1,20 @@
 <?php
 
 /**
- * Fichier de configuration de la classe ressource
+ * Fichier de configuration de la classe réssource
  */
 
 return (object) [
     // Liste des extensions valides en cas d'upload de fichier
-    "uploadFileExtension" => [ "jpg", "png", "jpeg", "ico", "git", "doc", "pdf" ],
+    "upload_file_extension" => [ "jpg", "png", "jpeg", "ico", "git", "doc", "pdf" ],
     // Répertoire d'upload de fichier, Deux types sont valides
     // :folder|:ftp
-    "uploadConfiguration" => (object) [
-        "type" => "folder",
-        "config" => [
-            "folder" => [
-                "dirname" => dirname(__DIR__) . "/resoures/public/upload"
-            ],
-            "ftp" => (object) [
-                "password" => "mot de passe ftp",
-                "username" => "nom d'utilisateur ftp",
-                "root" => "~",
-                "tls" => false, // A `true` pour activer une connection sécurisé.
-                "timeout" => 50 // Temps d'attente de connection
-            ]
-        ]
+    "upload_directory" => dirname(__DIR__) . "/resoures/public/upload",
+    "ftp" => [
+        "password" => "mot de passe ftp",
+        "username" => "nom d'utilisateur ftp",
+        "root" => "~",
+        "tls" => false, // A `true` pour activer une connection sécurisé.
+        "timeout" => 50 // Temps d'attente de connection
     ]
 ];
