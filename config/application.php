ab7d634d0d318e7611bb23b0f2d4c0af46d27745
Franck Dakia <dakiafranck@gmail.com>
2016-03-11 23:38:05 +0000

    Ajout de la AppConfiguration::template_extension

--- config/application.php
+++ config/application.php
@@ -10,34 +10,40 @@
 */
 
 return (object) [
-    
+
     // Nom de l'Application
 	"app_name" => "The Bow Framework",
-    
+
+    // Racine de l'application
+    "app_root" => "/app",
+
     // La local
 	"timezone" => "Africa/Abidjan",
-    
+
     // Repertoire de log
     "log_direcotory_name" => dirname(__DIR__) . "/storage/logs",
-    
+
     // En cas d'utilisation de token. Ceci est le temps de vie d'un token.
     // il est vivement conseil de programmer avec des tokens.
 	"token_expirate_time" => 50000,
-    
+
     // chemin du fichier 404
     // Quand une uri est invalide, alors Bow
     // chargera ce fichier.
 	"not_found_file_name" => dirname(__DIR__) . "/app/views/404.php",
-    
-    // Template par defaut utiliser par Snoop. Le systeme implement 3 moteur de template
-    // Valeur possible: twig, mustache, jade
-  	"template" => "twig",
-    
+
+    // Template par defaut utiliser par Bow. Le systeme implement 3 moteur de template
+    // Valeur possible: twig, mustache, jade-php
+  	"template_engine" => "twig",
+
+	// extension des fichiers de template.
+	"template_extension" => ".php",
+
     // Le repertoire des vues. C'est dans ce repertoire que
     // vous allez mettre tous vos vues.
     // Les vues doivent avoir une instantion *.php
     "views" => dirname(__DIR__) . "/app/views",
-    
+
     // Le repertoire de cache.
     // quand le cache sera remplit c'est à vous de le vidé
     "cache_folder" => dirname(__DIR__) . "/storage/cache",
@@ -52,7 +58,7 @@ return (object) [
             "middleware" => "App\\Middleware",
             "app"        => "App\\Autoload",
         ],
-        
+
         // Liste de middleware
         // * ici quand vous générez un middleware
         // * il faudra l'enregistré dans ce tableau
