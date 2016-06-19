4f613a79027df318584f776bca5efa6e0cb9d3ef
Franck Dakia <dakiafranck@gmail.com>
2016-06-19 15:03:11 +0000

    Refonte de routes.php

---
+++ app/routes.php
@@ -0,0 +1,17 @@
+<?php
+/**-------------------------------------------------------------------------------------------------
+| Chargement des routes
+|---------------------------------------------------------------------------------------------------
+|	Chargement des routes
+| 	=====================
+| 	Voici où vous pouvez enrégistrer toutes les routes pour une application.
+| 	C'est un jeu d'enfant vous verez. Il suffit de dire à Bow les URI aux
+|	quelles il doit répondre et de lui donner le contrôleur à appeler
+| 	lorsque cet URL est demandée.
+|
+|   Suivez l'exemple suivant, il vous donne un aperçu sur comment ça fonction en général.
+*/
+
+$app->get("/", function () {
+	view("welcome");
+});
