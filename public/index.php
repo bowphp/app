404bd665c91cd95ba0c27e9194748ea4afecee24
Franck Dakia <dakiafranck@gmail.com>
2016-06-19 15:02:00 +0000

    deplacement de routes et refonte de index.php

--- public/index.php
+++ public/index.php
@@ -4,13 +4,13 @@
 |	CHARGEMENT DES CONFIGURATIONS
 |	CHARGEMENT DES PRINCIPAUX FICHIERS DE L'APPLICATION BOW
 */
-require_once dirname(__DIR__) . "/vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 // Création de l'application
 $app = Bow\Core\Application::configure(config());
 
 // Chargement des routeurs.
-require dirname(__DIR__) . "/app/Router/routes.php";
+require __DIR__ . "/../app/routes.php";
 
 // Lancement de l'application
 $app->run();
