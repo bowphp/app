92041a1be0c1a7c38f9dc92bcee2cab015b68c00
Franck Dakia <dakiafranck@gmail.com>
2016-07-31 17:36:03 +0000

    MAJ de la config

--- config/resource.php
+++ config/resource.php
@@ -2,16 +2,18 @@
 /**
  * Fichier de configuration de la classe réssource
  */
-return (object) [
-    // Store locatin
-    "storage_directory" => __DIR__."/../storage/app",
+return [
+    // Store location
+    'storage' => __DIR__.'/../storage/app',
 
     // FTP. configuration
-    "ftp" => [
-        "password" => "mot de passe ftp",
-        "username" => "nom d'utilisateur ftp",
-        "root" => "~",
-        "tls" => false, // A `true` pour activer une connection sécurisé.
-        "timeout" => 50 // Temps d'attente de connection
+    'ftp' => [
+        'hostname' => 'localhost',
+        'password' => 'mot de passe ftp',
+        'username' => 'nom d\'utilisateur ftp',
+        'port'     => 21,
+        'root' => null,
+        'tls' => false, // A `true` pour activer une connection sécurisé.
+        'timeout' => 50 // Temps d'attente de connection
     ]
 ];
