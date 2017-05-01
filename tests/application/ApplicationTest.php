cb4e290706f7e4e06b9c39bf1cb9e3510d6c965a
Franck Dakia <dakiafranck@gmail.com>
2017-05-01 16:54:24 +0000

    tests: Ajout de methode de test unitaire

--- tests/application/ApplicationTest.php
+++ tests/application/ApplicationTest.php
@@ -1,11 +1,40 @@
 <?php
 
-class ApplicationTest extends \PHPUnit\Framework\TestCase
+class ApplicationTest extends \Bow\Support\Test\BowTestCase
 {
+    /**
+     * @var string
+     */
+    protected $base_url = 'http://localhost:5000/';
+
+    /**
+     * @return mixed
+     */
     public function testApplicationInstance()
     {
         // @ permet juste de fixer un bugs du côté lancement session
         $app = @require __DIR__.'/../../public/index.php';
         $this->assertInstanceOf(\Bow\Application\Application::class, $app);
+        return $app;
+    }
+
+    /**
+     * @depends testApplicationInstance
+     */
+    public function testGetWelcome()
+    {
+        $this->visite('GET', '/')
+            ->statusCodeMustBe(200)
+            ->contentTypeMustBe('text/html');
+    }
+
+    /**
+     * @depends testApplicationInstance
+     */
+    public function testGetJsonForIUsersControllerIndexAction()
+    {
+        $this->visite('GET', '/json')
+            ->statusCodeMustBe(200)
+            ->contentTypeMustBeJson();
     }
 }
