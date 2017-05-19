323cfec46cba5afc031978399e4ee5efd028388d
Franck Dakia <dakiafranck@gmail.com>
2017-05-19 10:25:53 +0000

    config: refonte des initialisations

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -1,25 +1,31 @@
 <?php
+use \Bow\Application\Configuration;
+
 /**
  | Fichier de chargement global de configuration de l'application
  */
-\Bow\Application\Configuration::configure([
-    'application' => require __DIR__.'/application.php',
-    'database'    => require __DIR__.'/database.php',
-    'mail'        => require __DIR__.'/mail.php',
-    'resource'    => require __DIR__.'/resource.php',
-    'envfile'	  => __DIR__.'/../.envfile.json',
-    'app_base_dirname' => __DIR__.'/../'
+
+$config = Configuration::configure([
+	'envfile'	  => __DIR__.'/../.envfile.json',
+	'app_base_dirname' => __DIR__.'/../'
+]);
+
+$config->setAllConfiguration([
+	'application' => require __DIR__.'/application.php',
+ 	'database'    => require __DIR__.'/database.php',
+	'mail'        => require __DIR__.'/mail.php',
+	'resource'    => require __DIR__.'/resource.php'
 ]);
 
 /**
  * Configuration de la Request et de la Response
  */
-\Bow\Http\Response::configure(config('view path'));
+\Bow\Http\Response::configure($config->getViewpath());
 
 /**
  * Configuration de Mail.
  */
-\Bow\Mail\Mail::configure(config('mail'));
+\Bow\Mail\Mail::configure($config->getMailConfiguration());
 
 /**
  * Initialisation du token
@@ -29,29 +35,29 @@
 /**
  * Configuration de la Sécurité
  */
-\Bow\Security\Security::setkey(config('key'));
+\Bow\Security\Security::setkey($config->getAppkey());
 
 /**
  * Configuration de la base de donnée
  */
-\Bow\Database\Database::configure(config('db'));
+\Bow\Database\Database::configure($config->getDatabaseConfiguration());
 
 /**
  * Configuration du systeme de cache
  */
-\Bow\Http\Cache::confirgure(config('cache').'/bow');
+\Bow\Http\Cache::confirgure($config->getCachepath().'/bow');
 
 /**
  * Configuration de la resource de l'application
  */
-\Bow\Resource\Storage::configure(config('resource'));
+\Bow\Resource\Storage::configure($config->getResourceConfiguration());
 
 /**
  * Configuration du charger de vue
  */
-\Bow\View\View::configure(config());
+\Bow\View\View::configure($config);
 
 /**
  * Configuration de translator
  */
-\Bow\Translate\Translator::configure(config()->getDefaultLang(), config()->getTranslateDirectory());
+\Bow\Translate\Translator::configure($config->getDefaultLang(), $config->getTranslateDirectory());
