c4c6d7689c77febcae2a32ad98887854faf70868
Franck Dakia <dakiafranck@gmail.com>
2017-08-04 07:21:16 +0000

    Ajout de helper

--- config/helper.php
+++ config/helper.php
@@ -1,7 +1,14 @@
 <?php
 
-if (! function_exists('see_hello')) {
-    function see_hello() {
-        echo 'hello, world.';
+if (! function_exists('gen_slix')) {
+	/**
+	 * Génère un code aléatoire.
+	 * Peut être utiliser pour masquer le nom de champs de formulaire.
+	 * 
+	 * @param int $len
+	 * @return string
+	 */
+    function gen_slix($len = 4) {
+        return substr(str_shuffle(uniqid()), 0, $len);
     }
 }
