d12ac00591e4f3ecd289fb8c1b597a10aad1844e
Franck Dakia <dakiafranck@gmail.com>
2017-08-23 12:59:54 +0000

    Ajout de classe auth et authorisation

---
+++ framework/src/Auth/Auth.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Bow\Auth;
+
+class Auth
+{
+    /**
+     * Check if user is authenticate
+     *
+     * @return bool
+     */
+    public static function check()
+    {
+        return true;
+    }
+
+    /**
+     * Check if user is authenticate
+     *
+     * @return bool
+     */
+    public static function user()
+    {
+        return true;
+    }
+
+    /**
+     * Check if user is authenticate
+     *
+     * @return bool
+     */
+    public static function attempts()
+    {
+        return true;
+    }
+}
