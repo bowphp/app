370f94f26a2784bde99c63b61544d166608d5f38
Franck Dakia <dakiafranck@gmail.com>
2017-09-15 21:20:30 +0000

    Refonte de l'auth

--- framework/src/Auth/Auth.php
+++ framework/src/Auth/Auth.php
@@ -5,6 +5,16 @@ namespace Bow\Auth;
 class Auth
 {
     /**
+     * Configure Auth system
+     * 
+     * @param array $config
+     */
+    public static function configure(array $config)
+    {
+        static::$config = $config;
+    }
+
+    /**
      * Check if user is authenticate
      *
      * @return bool
@@ -29,8 +39,34 @@ class Auth
      *
      * @return bool
      */
-    public static function attempts()
+    public static function attempts(array $credentials)
     {
         return true;
+    }
+
+    /**
+     * Check if user is authenticate
+     *
+     * @return bool
+     */
+    public static function attempts(array $credentials)
+    {
+        return true;
+    }
+
+    /**
+     * __call
+     * 
+     * @param string $method
+     * @param array $parameters
+     * @return mixed
+     */
+    public function __call($method, $parameters)
+    {
+        if (method_exists(static::class, $method)) {
+            return call_user_func_array([static, $method], $parameters);
+        }
+
+        throw new BadMethodCallException("la methode $methode n'existe pas", 1);
     }
 }
