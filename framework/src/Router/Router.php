66bbdc4dc11effaa9a5174c9fc755d863b751e7c
Franck Dakia <dakiafranck@gmail.com>
2018-02-26 17:36:11 +0000

    [router] refonte du router

--- framework/src/Router/Router.php
+++ framework/src/Router/Router.php
@@ -49,4 +49,129 @@ class Router
     {
         return $this->collection;
     }
+
+
+    /**
+     * get, route de type GET ou bien retourne les variable ajoutés dans Bow
+     *
+     * @param string         $path
+     * @param callable|array $cb
+     *
+     * @return Route
+     */
+    public function get($path, $cb)
+    {
+        return $this->routeLoader('GET', $path, $cb);
+    }
+
+    /**
+     * post, route de type POST
+     *
+     * @param string   $path
+     * @param callable $cb
+     *
+     * @return Route
+     */
+    public function post($path, $cb)
+    {
+        return $this->routeLoader('POST', $path, $cb);
+    }
+
+    /**
+     * any, route de tout type GET|POST|DELETE|PUT|OPTIONS|PATCH
+     *
+     * @param string   $path
+     * @param Callable $cb
+     *
+     * @return Route
+     */
+    public function any($path, callable $cb)
+    {
+        foreach (['options', 'patch', 'post', 'delete', 'put', 'get'] as $method) {
+            $this->$method($path, $cb);
+        }
+
+        return $this;
+    }
+
+    /**
+     * delete, route de tout type DELETE
+     *
+     * @param string   $path
+     * @param callable $cb
+     *
+     * @return Route
+     */
+    public function delete($path, $cb)
+    {
+        return $this->pushHttpVerbe('DELETE', $path, $cb);
+    }
+
+    /**
+     * put, route de tout type PUT
+     *
+     * @param string   $path
+     * @param callable $cb
+     *
+     * @return Route
+     */
+    public function put($path, $cb)
+    {
+        return $this->pushHttpVerbe('PUT', $path, $cb);
+    }
+
+    /**
+     * patch, route de tout type PATCH
+     *
+     * @param string   $path
+     * @param callable $cb
+     *
+     * @return Route
+     */
+    public function patch($path, $cb)
+    {
+        return $this->pushHttpVerbe('PATCH', $path, $cb);
+    }
+
+    /**
+     * patch, route de tout type PATCH
+     *
+     * @param  string   $path
+     * @param  callable $cb
+     * @return Route
+     */
+    public function options($path, callable $cb)
+    {
+        return $this->pushHttpVerbe('OPTIONS', $path, $cb);
+    }
+
+    /**
+     * code, Lance une fonction en fonction du code d'erreur HTTP
+     *
+     * @param  int      $code
+     * @param  callable $cb
+     * @return Route
+     */
+    public function code($code, callable $cb)
+    {
+        $this->statusRoutes[$code] = $cb;
+        return $this;
+    }
+
+    /**
+     * match, route de tout type de method
+     *
+     * @param  array    $methods
+     * @param  string   $path
+     * @param  callable $cb
+     * @return Route
+     */
+    public function match(array $methods, $path, callable $cb = null)
+    {
+        foreach ($methods as $method) {
+            $this->routeLoader(strtoupper($method), $path, $cb);
+        }
+
+        return $this;
+    }
 }
