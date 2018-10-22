61d8ad9315416189247f8f72f2fda57fa4819b0f
Franck Dakia <dakiafranck@gmail.com>
2018-10-22 12:33:45 +0000

    formating

--- components/components/Example.js
+++ components/components/Example.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 export default class Example extends Component {
-    render() {
-        return (
-            <div>
-                I'm an example component.
-            </div>
-        );
-    }
+	render() {
+		return (
+			<div>
+				I'm an example component.
+			</div>
+		);
+	}
 }
 
 if (document.getElementById('root')) {
-    ReactDOM.render(<Example />, document.getElementById('root'));
+	ReactDOM.render(<Example />, document.getElementById('root'));
 }
