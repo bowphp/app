703c34847cbc665b233579b99cbcaea3fa888d40
Franck DAKIA <dakiafranck@gmail.com>
2018-12-11 14:23:51 +0000

    [mix] formating

--- Mixfile.js
+++ Mixfile.js
@@ -1,21 +1,21 @@
 module.exports = {
-	config: {
-		prefix: __dirname,
-		version: false,
-		versionFilename: __dirname + '/.mixfile-version.json'
-	},
+  config: {
+    prefix: __dirname,
+    version: false,
+    versionFilename: __dirname + '/.mixfile-version.json'
+  },
 
   /**
    * React, vue and Vanilla Javascript compile files
    */
   javascript: [
-    ["components/js/app.js", "public/js"]
+    // ["components/js/app.js", "public/js"]
   ],
 
-	/**
-	 * Sass compile files
-	 */
-	sass: [
-	 	["components/sass/app.scss", "public/css"]
-	]
+  /**
+   * Sass compile files
+   */
+  sass: [
+    ["components/sass/app.scss", "public/css"]
+  ]
 };
