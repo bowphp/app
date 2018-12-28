91e94ba9946a622c396e2a6ad516d3db084adbcf
Franck DAKIA <dakiafranck@gmail.com>
2018-12-28 23:23:38 +0000

    refonte initial view

--- components/views/errors/404.tintin.php
+++ components/views/errors/404.tintin.php
@@ -1,36 +1,13 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Lato:400" type="text/css"/>
-    <link rel="icon" type="image/x-icon" href="/favicon.png"/>
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.png">
-    <title>Bow - worked</title>
-    <style type="text/css">
-        div#main {
-          position: relative;
-          margin: 10% auto;
-          font-size: 15px;
-          width: 550px;
-          text-align: center;
-          font-family: "Lato", "console", monospace, serif, "sans-serif";
-        }
+#extends('layout')
 
-        h1 {
-          color: #bd362f;
-          text-decoration: none;
-        }
-    </style>
-</head>
-<body>
-<div id="main">
-    <div>
-        <h1>404</h1>
-        <p>
-            Not found page
-        </p>
-    </div>
+#block('title', '404 - Not Found')
+
+#block('content')
+<div>
+    <h1>404</h1>
+    <p>
+        Not found page <br>
+        <a href="/">Back</a>
+    </p>
 </div>
-</body>
-</html>
+#endblock
--- components/views/welcome.tintin.php
+++ components/views/welcome.tintin.php
@@ -1,35 +1,14 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta http-equiv="content-type" content="text/html; charset=utf-8"/>
-    <meta name="csrf-token" content="{{ csrf_token() }}"/>
-    <link rel="icon" type="image/x-icon" href="/favicon.png"/>
-    <link rel="shortcut icon" type="image/x-icon" href="/favicon.png">
-    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
-    <title>It's Worked</title>
-    <style type="text/css">
-        div#main {
-          position: relative;
-          margin: 10% auto;
-          font-size: 15px;
-          width: 550px;
-          text-align: center;
-          font-family: "Montserrat", "console", monospace, serif, "sans-serif";
-        }
+#extends('layout')
 
-        a {
-          color: #bd362f;
-          text-decoration: none;
-        }
-    </style>
-</head>
-<body>
-<div id="main">
-    <!-- <Example></Example> -->
-    <div>
-        I'm a bow application, you can see the <a href="https://bowphp.github.io">documentation</a>.
-    </div>
+#block('title', 'It\'s Worked')
+
+#block('content')
+<!-- <Example></Example> -->
+<div>
+    I'm a bow application, you can see the <a href="https://bowphp.github.io">documentation</a>.
 </div>
-<script type="text/javascript" src="/js/app.js"></script>
-</body>
-</html>
+#endblock
+
+#block('script')
+    <script type="text/javascript" src="/js/app.js"></script>
+#endblock
