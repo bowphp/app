7819bf4cf52dc6dfe1f90d069ab243d83d28499a
Franck DAKIA <dakiafranck@gmail.com>
2019-02-16 14:00:22 +0000

    formating

--- app/Kernel.php
+++ app/Kernel.php
@@ -68,6 +68,8 @@ class Kernel extends ApplicationLoader
 
     /**
      * Service Bootstrap
+     * 
+     * @return void
      */
     public function boot()
     {
--- app/Middleware/Guest.php
+++ app/Middleware/Guest.php
@@ -12,6 +12,7 @@ class Guest
      *
      * @param  Request $request
      * @param  callable $next
+     * 
      * @return mixed
      */
     public function process(Request $request, callable $next)
