5d3f020db65e98a711e85ab982f1969ddf128798
Franck DAKIA <dakiafranck@gmail.com>
2019-03-15 23:47:05 +0000

    add error handler

---
+++ app/ErrorHandle.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Exception;
+
+class ErrorHandle
+{
+    /**
+     * handle the error
+     * 
+     * @param Exception $exception
+     * @return void
+     */
+    public function handle($exception)
+    {
+        // 
+    }
+}
