09304a69c8d8c77f8d6dc2734b6a4d16dee66286
Franck Dakia <dakiafranck@gmail.com>
2019-03-20 17:39:47 +0000

    [database] update user default seeding

--- database/seeders/users_seeder.php
+++ database/seeders/users_seeder.php
@@ -1,23 +1,23 @@
 <?php
-
 /**
  * The users seeder
  *
  * @see https://github.com/fzaninotto/Faker for all documentation
  */
+$faker = \Faker\Factory::create();
 
 $seeds['users'] = [];
 
 foreach (range(1, 5) as $key) {
     $seeds['users'][] = [
-        'id' => 1,
+        'id' => null,
         'name' => $faker->name,
         'description' => $faker->text,
         'email' => $faker->email,
-        'pseudo' => $faker->pseudo,
+        'pseudo' => uniqid(),
         'password' => $faker->password,
-        'created_at' => $faker->dateTime(),
-        'updated_at' => $faker->dateTime()
+        'created_at' => date('Y-m-d H:i:s'),
+        'updated_at' => date('Y-m-d H:i:s')
     ];
 }
 
