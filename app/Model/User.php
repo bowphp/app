0ef6e42c98d25dca8b881be317961ca5db76864e
Franck Dakia <dakiafranck@gmail.com>
2019-04-18 15:52:50 +0000

    add hidden field in user model

--- app/Model/User.php
+++ app/Model/User.php
@@ -6,5 +6,14 @@ use Bow\Auth\Authentication as Model;
 
 class User extends Model
 {
+    /**
+     * The list of hidden field when toJson is called
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password'
+    ];
+    
     // Do something
 }
