ae64283307d95537e51c5e66321538c65eae346e
Franck DAKIA <dakiafranck@gmail.com>
2019-06-16 13:34:17 +0000

    refonte error handler
    - Add more method for catch
    - Fix issue https://github.com/bowphp/framework/issues/48 #48

--- app/ErrorHandle.php
+++ app/ErrorHandle.php
@@ -2,18 +2,72 @@
 
 namespace App;
 
+use Bow\Database\Exception\NotFoundException;
 use Exception;
 
 class ErrorHandle
 {
     /**
      * handle the error
-     * 
+     *
      * @param Exception $exception
      * @return void
      */
     public function handle($exception)
     {
-        // 
+        if ($exception instanceof NotFoundException) {
+            return $this->render('errors.404', 404);
+        }
+    }
+
+    /**
+     * Render view as response
+     *
+     * @param string $view
+     * @param array $data
+     * @return mixed
+     */
+    public function render($view, $data = [], $code = 200)
+    {
+        if (is_numeric($data)) {
+            $code = $data;
+            $data = [];
+        }
+
+        $content = view($view, $data, $code)->getContent();
+
+        $this->send($content);
+    }
+
+    /**
+     * Send the json as response
+     *
+     * @param string $data
+     * @param array $code
+     * @return mixed
+     */
+    public function json($exception)
+    {
+        $data = [
+            'status' => [
+                'message' => $exception->getMessage(),
+                'success' => false
+            ], 'data' => $exception->getTrace()
+        ];
+
+        $content = json($data, $exception->getCode());
+
+        $this->send($content);
+    }
+
+    /**
+     * Die the process with content
+     *
+     * @param string $content
+     * @return mixed
+     */
+    private function send($content)
+    {
+        die($content);
     }
 }
