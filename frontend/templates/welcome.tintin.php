e7aa978e698daaa5f0890db741c4748d1bd51230
Bi Stephane <bistephaneta@gmail.com>
2020-07-07 22:59:26 +0000

    remove #block('script') section

--- frontend/templates/welcome.tintin.php
+++ frontend/templates/welcome.tintin.php
@@ -8,7 +8,3 @@
     I'm a bow application, you can see my <a href="https://bowphp.github.io">documentation</a>.
 </div>
 #endblock
-
-#block('script')
-    {# <script type="text/javascript" src="/js/app.js"></script> #}
-#endblock
