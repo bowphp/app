e131addea2c848772c9c977c5e363490b00fa466
Bi Stephane <bistephaneta@gmail.com>
2020-07-08 00:13:55 +0000

    fix(#24): remove the comment

--- frontend/templates/welcome.tintin.php
+++ frontend/templates/welcome.tintin.php
@@ -3,7 +3,6 @@
 #block('title', 'It\'s Worked')
 
 #block('content')
-<!-- <Example></Example> -->
 <div>
     I'm a bow application, you can see my <a href="https://bowphp.github.io">documentation</a>.
 </div>
