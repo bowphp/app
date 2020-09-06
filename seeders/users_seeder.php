43fd7c77ca916f379b498826b3ed9bf4343b210f
Franck DAKIA <dakiafranck@gmail.com>
2020-09-06 14:00:46 +0200

    change: refonte users seeder

--- seeders/users_seeder.php
+++ seeders/users_seeder.php
@@ -6,17 +6,17 @@
  */
 $faker = \Faker\Factory::create();
 
-$seeds['users'] = [];
+$seeds = [];
 
 foreach (range(1, 5) as $key) {
-    $seeds['users'][] = [
+    $seeds[] = [
         'name' => $faker->name,
         'description' => $faker->text,
         'email' => $faker->email,
-        'password' => bow_hash('password'), // password
+        'password' => bow_hash('password'),
         'created_at' => date('Y-m-d H:i:s'),
         'updated_at' => date('Y-m-d H:i:s'),
     ];
 }
 
-return $seeds;
+return ['users' => $seeds];
