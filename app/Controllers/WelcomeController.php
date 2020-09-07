f1f1c004101ce4cf501931445a9976847240bad5
Franck DAKIA <dakiafranck@gmail.com>
2020-09-07 23:32:34 +0200

    change: formating

--- app/Controllers/WelcomeController.php
+++ app/Controllers/WelcomeController.php
@@ -15,6 +15,6 @@ class WelcomeController extends Controller
      */
     public function __invoke(Request $request)
     {
-        return response()->render('welcome');
+        return $this->render('welcome');
     }
 }
