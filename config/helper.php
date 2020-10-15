f5c5263e8264177543391f9f2feaa0993b3c2c23
Franck DAKIA <dakiafranck@gmail.com>
2020-10-15 11:28:17 +0000

    feat: add create uuid helper

--- config/helper.php
+++ config/helper.php
@@ -357,3 +357,21 @@ if (! function_exists('gen_unique_id')) {
         return $id;
     }
 }
+
+if (!function_exists('create_guid')) {
+    /**
+     * Create the new uuid
+     *
+     * @return string
+     */
+    function create_guid()
+    {
+        return sprintf('%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff),
+            mt_rand(0, 0xffff),
+            mt_rand(0, 0x0fff) | 0x4000,
+            mt_rand(0, 0x3fff) | 0x8000,
+            mt_rand(0, 0xffff), mt_rand(0, 0xffff), mt_rand(0, 0xffff)
+        );
+    }
+  }
