f0acecf447125bf029c8b8d3f6cb67cb9002ba9d
Franck DAKIA <dakiafranck@gmail.com>
2023-05-24 16:37:18 +0000

    Update the policer config

--- config/policier.php
+++ config/policier.php
@@ -14,6 +14,13 @@ return [
     "iss" => app_env("APP_JWT_ISSUER", "app.example.com"),
 
     /**
+     * Hashing algorithm being used
+     *
+     * HS256, HS384, HS512, ES256, ES384, ES512
+     */
+    "alg" => "HS512",
+
+    /**
      * Configures the audience
      */
     "aud" => app_env("APP_JWT_AUD", "app.example.com"),
