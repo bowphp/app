acdbd5cf46fb8fd5539388dab411c31e498b66cc
Franck DAKIA <dakiafranck@gmail.com>
2024-12-16 23:30:21 +0000

    fix: error handler return result

--- app/Exceptions/ErrorHandle.php
+++ app/Exceptions/ErrorHandle.php
@@ -5,7 +5,6 @@ namespace App\Exceptions;
 use Exception;
 use Bow\Http\Exception\HttpException;
 use Bow\Application\Exception\BaseErrorHandler;
-use Bow\Http\Exception\ResponseException as HttpResponseException;
 use Bow\Database\Exception\NotFoundException as ModelNotFoundException;
 
 class ErrorHandle extends BaseErrorHandler
@@ -26,17 +25,16 @@ class ErrorHandle extends BaseErrorHandler
             || $exception instanceof HttpException
         ) {
             $code = $exception->getStatusCode();
+
             return $this->render('errors.' . $code, [
                 'code' => 404,
                 'exception' => $exception
             ]);
         }
 
-        if ($exception instanceof HttpResponseException) {
-            return $this->render('errors.500', [
-                'code' => 404,
-                'exception' => $exception
-            ]);
-        }
+        return $this->render('errors.500', [
+            'code' => 404,
+            'exception' => $exception
+        ]);
     }
 }
